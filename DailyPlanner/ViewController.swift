6c2023ef0531c03acb4e8e2c4826ca1b80608aea
Sam Ziegler <samziegler@sams-macbook-pro-2.local>
2019-02-25 16:22:46 -0600

    Initial Commit

---
+++ DailyPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by Sam Ziegler on 2/25/19.
+//  Copyright © 2019 Sam Ziegler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
