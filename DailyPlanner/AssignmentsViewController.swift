0fa9980795316552e7a94bdc794cca95b5cc68f7
Sam Ziegler <samuel.t.ziegler@outlook.com>
2020-08-20 10:02:58 -0500

    Adding and committing a view controller for the assignments screen

---
+++ DailyPlanner/AssignmentsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by Sam Ziegler on 2/25/19.
+//  Copyright © 2019 Sam Ziegler. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle{return .lightContent}
+}
