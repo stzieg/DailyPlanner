1db968039c5b1a1dc3c4802abc137fdcb63c9dd0
Sam Ziegler <samuel.t.ziegler@outlook.com>
2020-08-24 20:27:27 -0500

    committing view

--- DailyPlanner/ViewControllers/AddAssignmentsViewController.swift
+++ DailyPlanner/ViewControllers/AddAssignmentsViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class AddAssignmentsViewController: UIViewController {
     
+//    @IBOutlet weak var NotesBottomConstraint: NSLayoutConstraint!
+    
+//    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+//        self.view.endEditing(true)
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Add assignments screen loaded")
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
     }
     override var preferredStatusBarStyle: UIStatusBarStyle{return .lightContent}
+    
+//    @objc func keyboardWillShow(notification:NSNotification){
+//        if let info = notification.userInfo{
+//            let rect:CGRect = info["UIKeyboardFrameEndUserInfoKey"] as! CGRect
+//            self.view.layoutIfNeeded()
+//            UIView.animate(withDuration: 0.25, animations: {
+//                self.view.layoutIfNeeded()
+//                self.NotesBottomConstraint.constant = rect.height + 20
+//            })
+//        }
+//    }
 }
