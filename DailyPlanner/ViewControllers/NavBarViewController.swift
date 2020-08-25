b1f3e52c3ae04f27a9f3cd9f066ad2f7a98ba36d
Sam Ziegler <samuel.t.ziegler@outlook.com>
2020-08-24 20:26:03 -0500

    Nav bar

---
+++ DailyPlanner/ViewControllers/NavBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NavBarViewController.swift
+//  DailyPlanner
+//
+//  Created by Sam Ziegler on 8/21/20.
+//  Copyright © 2020 Sam Ziegler. All rights reserved.
+//
+
+import UIKit
+
+class NavBarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle{return .lightContent}
+    
+}
